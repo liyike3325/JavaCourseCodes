8b48afca80173123f930491ff6c77a7176a9244f
mac <liyike3325@163.com>
2021-09-17 16:05:25 +0800

    update xlassloader

---
+++ 01jvm/jvm02/xlassloader/src/test/java/com/jvm/xlassloader/XlassloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.xlassloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlassloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
