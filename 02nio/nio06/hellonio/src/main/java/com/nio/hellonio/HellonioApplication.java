c203ddfb5adb2d0d634754dd41b46c0bf7b7086c
mac <liyike3325@163.com>
2021-09-24 16:36:27 +0800

    add nio06

---
+++ 02nio/nio06/hellonio/src/main/java/com/nio/hellonio/HellonioApplication.java
@@ -0,0 +1,13 @@
+package com.nio.hellonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellonioApplication.class, args);
+	}
+
+}
---
+++ 02nio/nio06/hellonio/src/test/java/com/nio/hellonio/HellonioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nio.hellonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
